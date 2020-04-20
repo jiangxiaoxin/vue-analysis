bde23521f4571de34f39a3874d8aff974ff49fff
helloca <815612516@qq.com>
2020-04-20 10:07:43 +0800

    update

--- htmls/webpack.config.js
+++ htmls/webpack.config.js
@@ -1,9 +1,34 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.join(__dirname, "./src/index.js"),
-    output: {
-        path: path.join(__dirname, "./dist"),
-        filename: "bundle.js"
-    }
+  entry: path.join(__dirname, "./src/index.js"),
+  output: {
+    path: path.join(__dirname, "./bundle"),
+    filename: "bundle.[hash:6].js"
+  },
+  module: {
+    rules: [
+      {
+        test:  /\.(png|jpe?g|gif)$/i,
+        loader: "file-loader",
+        options: {
+          name: '[name].[hash:10].[ext]',
+          outputPath: 'images'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      {
+        template: path.resolve(__dirname, "index.html"),
+        filename: 'index.[hash:6].html'
+      }
+    ),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'component.html'),
+      filename: 'component.[hash:10].html'
+    })
+  ]
 }
